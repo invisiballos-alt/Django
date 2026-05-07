28bbd09fb996acd57b9328eb9ddd668179136acd
Dmitry <mr.timy297@yandex.ru>
2026-05-07 20:25:56 +0500

    Casino v1.0

--- main/views.py
+++ main/views.py
@@ -5,5 +5,6 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'main/index.html')
 
-def about(request):
-    return HttpResponse("<h1>AOBUT!</h1>")
+def casino(request):
+    return render(request, 'main/casino.html')
+
